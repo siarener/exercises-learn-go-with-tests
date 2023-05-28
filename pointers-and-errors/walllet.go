b5922d0d5a693e60a76c35c430121c2c56326c5f
apfelkraefpla <unknown>
2023-05-28 23:06:35 +0200

    feat: add pointers and errors

---
+++ pointers-and-errors/walllet.go
@@ -0,0 +1,43 @@
+package wallet
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Balance() Bitcoin {
+	/*
+		use a pointer in the receiver, although we only need
+		a copy of Wallet, as it is convention to keep your method receiver
+		types the same for consistency
+	*/
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
