1907bae41d6bb4dff175824aaaa3d6ca0e5d84ff
apfelkraefpla <>
2023-05-29 15:03:53 +0200

    feat: add maps

---
+++ maps/dictionary.go
@@ -0,0 +1,69 @@
+package dictionary
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	/*
+		no pointer is needed here to modify the map, as
+		a map value is already a pointer to a runtime.hmap
+		struture;
+		i.e. when passing a map to a function/method you are
+		copying the pointer, not the underlying data
+		structure containing the data
+	*/
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	/*
+		no additional error handling necessary, as deleting
+		a non existing value has no effect
+	*/
+	delete(d, word)
+}
