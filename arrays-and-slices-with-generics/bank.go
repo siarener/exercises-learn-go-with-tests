014051224a73ddcd9b55a94400a01aa8b675c8c4
apfelkraefpla <>
2023-06-20 22:03:50 +0200

    feat: :construction: add working code, ready to challenge Monzo, Barclays, et al.

---
+++ arrays-and-slices-with-generics/bank.go
@@ -0,0 +1,19 @@
+package sum
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		} else if t.To == name {
+			balance += t.Sum
+		}
+	}
+	return balance
+}
---
+++ arrays-and-slices-with-generics/sum_test.go
@@ -0,0 +1,84 @@
+package sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	numbers := []int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		/* The default value '1' is used as identity element,
+		as when multiplying with our initial value by that element
+		will result in an unchanged value.
+		In contrast, using '0' (i.e. the identity element for additions)
+		would lead to mupliplications by '0', and the final result
+		would always be '0'.
+		*/
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{From: "Elke", To: "Riya", Sum: 100},
+		{From: "Adil", To: "Elke", Sum: 25},
+	}
+
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Elke"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+}
